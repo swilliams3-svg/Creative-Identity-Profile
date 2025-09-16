fcbd4f2585193b09def694ad11791304b3f5c31b
swilliams3-svg <s.williams3@bolton.ac.uk>
2025-09-16 18:55:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,7 @@ elif page == "results":
     # Academic Section
     # --------------------------
     with st.expander("The Science Behind the Creative Identity & Personality Profile"):
-        with open("academic_section.txt", "r") as f:
+        with open("academic_article.txt", "r") as f:
             st.markdown(f.read())
 
     # --------------------------
