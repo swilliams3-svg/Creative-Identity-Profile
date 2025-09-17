bc5825daa283495e94c1aa5b48e6ecc4e9aed371
swilliams3-svg <s.williams3@bolton.ac.uk>
2025-09-17 13:24:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -543,7 +543,7 @@ elif st.session_state.page == "results":
         with open("academic_article.txt", "r") as f:
             st.markdown(f.read())
 
-    from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, ListFlowable, ListItem
 from reportlab.lib.enums import TA_LEFT
